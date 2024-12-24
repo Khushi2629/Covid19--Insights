0b363762867a94d382a5891be61d2b9abf85492b
Khushi2629 <121286189+Khushi2629@users.noreply.github.com>
2024-12-24 13:41:13 +0530

    Update make_mortality.py

--- Components/make_mortality.py
+++ Components/make_mortality.py
@@ -43,8 +43,8 @@ def make_mortality(in_path, out_path):
 
 if __name__ == '__main__':
 
-    in_path = './data/processed'
-    out_path = './data/processed'
+    in_path = './data_set/processed'
+    out_path = './data_set/processed'
 
     make_mortality(in_path=in_path,
                    out_path=out_path)    

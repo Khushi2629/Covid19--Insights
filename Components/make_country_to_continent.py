d301aa28f6ae95e1e9cae30c9efd7c52243d6273
Khushi2629 <121286189+Khushi2629@users.noreply.github.com>
2024-12-24 13:40:42 +0530

    Update make_country_to_continent.py

--- Components/make_country_to_continent.py
+++ Components/make_country_to_continent.py
@@ -23,8 +23,8 @@ def make_country_to_continent(in_path, out_path):
 
 if __name__ == '__main__':
 
-    in_path = './data/processed'
-    out_path = './data/processed'
+    in_path = './data_set/processed'
+    out_path = './data_set/processed'
 
     make_country_to_continent(in_path=in_path,
                    out_path=out_path)            
